006f50f991049b53ba2026954fcf55eae11678b0
subrattech <beherasubrat5040@gmail.com>
2025-04-29 14:48:36 +0530

    jjj

---
+++ src/test/java/com/cyfrifpro/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyfrifpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
